f3cd4ec8376de921f50777a0e0b424bf8cc7911c
ztratify <strat.barrett@shopify.com>
2021-08-15 15:41:31 -0700

    Deploy 🎉 Heroku + Digital Ocean

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const prodEndpoint = 'https://habits-backend-server.herokuapp.com/api/graphql';
+export const prodEndpoint = 'https://habits-api.stratbarrett.dev/api/graphql';
 export const devEndpoint = 'http://localhost:3000/api/graphql';
